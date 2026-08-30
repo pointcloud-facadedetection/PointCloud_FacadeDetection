3bd3c657320a9a084ab464fe9acd0e9958577016
Ruiqi Zhang <ruyiDorothy@163.com>
2026-08-30 11:17:19 +0800

    ICP算法

--- facadeDetection/algorithms/registration/__init__.py
+++ facadeDetection/algorithms/registration/__init__.py
@@ -1,3 +1,9 @@
-from .point_to_plane_icp import ICPResult, point_to_plane_icp
+from .point_to_plane_icp import (
+    ICPResult, point_to_plane_icp, rigid_transform_from_correspondences,
+    manual_seeded_icp, RegistrationCloud, build_registration_cloud,
+    registration_metrics
+)
 
-__all__ = ['ICPResult', 'point_to_plane_icp']
+__all__ = ['ICPResult', 'point_to_plane_icp', 'rigid_transform_from_correspondences',
+            'manual_seeded_icp', 'RegistrationCloud', 'build_registration_cloud',
+            'registration_metrics']
