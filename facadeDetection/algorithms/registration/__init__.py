eb44fca7c850316d7a5259c88955ef534552a068
Ruiqi Zhang <ruyiDorothy@163.com>
2026-08-31 15:43:17 +0800

    基础算法修正

--- facadeDetection/algorithms/registration/__init__.py
+++ facadeDetection/algorithms/registration/__init__.py
@@ -1,9 +1,12 @@
 from .point_to_plane_icp import (
     ICPResult, point_to_plane_icp, rigid_transform_from_correspondences,
     manual_seeded_icp, RegistrationCloud, build_registration_cloud,
-    registration_metrics
+    registration_metrics, RegistrationConfig
 )
+from .global_transform import GlobalTransformAudit, audit_exported_global_transform
 
 __all__ = ['ICPResult', 'point_to_plane_icp', 'rigid_transform_from_correspondences',
             'manual_seeded_icp', 'RegistrationCloud', 'build_registration_cloud',
+            'GlobalTransformAudit', 'audit_exported_global_transform',
+            'RegistrationConfig',
             'registration_metrics']
